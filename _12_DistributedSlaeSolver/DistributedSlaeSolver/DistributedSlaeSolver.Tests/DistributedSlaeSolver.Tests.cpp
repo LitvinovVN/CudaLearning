c94aa35f1b22951a0647ab10be7477a8c1f87689
GrachevaNN <grann72@mail.ru>
2021-04-27 09:46:57 +0300

    123

---
+++ _12_DistributedSlaeSolver/DistributedSlaeSolver/DistributedSlaeSolver.Tests/DistributedSlaeSolver.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DistributedSlaeSolverTests
+{
+	TEST_CLASS(DistributedSlaeSolverTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
