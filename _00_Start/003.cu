23e66a0947013d5c8f076ea78ba763783d03ee92
Vladimir Litvinov <litvinovvn@rambler.ru>
2022-10-08 13:59:34 +0300

    08.10.2022_13:59:34.25

--- _00_Start/003.cu
+++ _00_Start/003.cu
@@ -1,14 +1,16 @@
-// Задача 003. Передать в CUDA-ядро число типа float. Увеличить его на 1.5.
+// Задача 003. Передать в CUDA-ядро число типа float.
+// Увеличить его на 1.5. Скопировать измененное значение из GPU в ОЗУ.
+// Вывести измененное значение переменной в консоль.
 // Запуск:
-// nvcc 002.cu
+// nvcc 003.cu
 // ./a
 
 #include <iostream>
 
 __global__ void cuda_hello(float* dev_varFloat){    
-    printf("cuda_hello START: varFloat = %f\n", dev_varFloat);
-    dev_varFloat += 1.5;
-    printf("cuda_hello END: varFloat = %f\n", dev_varFloat);
+    printf("cuda_hello START: varFloat = %f\n", *dev_varFloat);
+    *dev_varFloat += 1.5;
+    printf("cuda_hello END: varFloat = %f\n", *dev_varFloat);
 }
 
 int main() {
@@ -16,9 +18,11 @@ int main() {
     printf("main START: varFloat = %f\n", varFloat);
 
     float* dev_varFloat;
+    cudaMalloc((void**)&dev_varFloat, sizeof(float));
+    cudaMemcpy(dev_varFloat, &varFloat, sizeof(float), cudaMemcpyHostToDevice);
 
     cuda_hello<<<1,1>>>(dev_varFloat);
-    cudaDeviceSynchronize();// Ожидание основным потоком выполнения функции cuda_hello
+    cudaMemcpy(&varFloat, dev_varFloat, sizeof(float), cudaMemcpyDeviceToHost);
     
     printf("main END: varFloat = %f\n", varFloat);
 
---
+++ _00_Start/004.cu
@@ -0,0 +1,52 @@
+// Задача 004.
+// Создать массив из десяти чисел типа float.
+// Скопировать значения элементов массива из ОЗУ в GPU.
+// Передать в CUDA-ядро массив из десяти чисел типа float.
+// Увеличить значения кождого из элементов массива на 1.5.
+// Скопировать значения элементов массива из GPU в ОЗУ.
+// Вывести значения элементов массива в консоль.
+// Запуск:
+// nvcc 004.cu
+// ./a
+
+#include <iostream>
+
+__global__ void cuda_array_add(float* dev_arrFloat, int size){ 
+    printf("----- cuda_array_add START -----\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("i = %d: %f + 1.5 = ", i, dev_arrFloat[i]);
+        dev_arrFloat[i] += 1.5;
+        printf("%f\n", dev_arrFloat[i]);
+    }        
+    printf("----- cuda_array_add END -----\n");
+}
+
+int main() {
+    int N = 10;
+
+    float* arrFloat = (float*)malloc(N * sizeof(float));
+    for(int i = 0; i < N; i++)
+    {
+        arrFloat[i] = i;
+        printf("arrFloat[%i] = %f\n", i, arrFloat[i]);
+    }
+
+    float* dev_arrFloat;
+    cudaMalloc((void**)&dev_arrFloat, N * sizeof(float));
+    cudaMemcpy(dev_arrFloat, arrFloat, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    cuda_array_add<<<1,1>>>(dev_arrFloat, N);
+    cudaDeviceSynchronize();
+    cudaError_t err1 = cudaMemcpy(arrFloat, dev_arrFloat, N * sizeof(float), cudaMemcpyDeviceToHost);
+    printf(cudaGetErrorString (err1));
+    cudaDeviceSynchronize();
+
+    printf("-----------------------\n");
+    for(int i = 0; i < N; i++)
+    {        
+        printf("arrFloat[%i] = %f\n", i, arrFloat[i]);
+    }
+
+    return 0;
+}
